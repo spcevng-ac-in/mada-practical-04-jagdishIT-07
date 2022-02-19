3f3e0780a0f79468c3682250502527a7193c66f3
jagdishIT-07 <thakorjagdish.fsd@gmail.com>
2022-02-19 16:33:56 +0530

    Practical 4 Submission

---
+++ app/src/main/java/com/example/firstactivity/MainActivity2.java
@@ -0,0 +1,13 @@
+ package com.example.firstactivity;
+ import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+ public class MainActivity2 extends AppCompatActivity {
+  private TextView textview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
